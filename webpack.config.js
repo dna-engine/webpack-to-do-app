407c3b358953ea8c8087af3f3e79bb8af62ceba5
Dem Pilafian <dem@centerkey.com>
2017-02-24 21:41:25 -0800

    Project definition and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+// dna.js webpack To Do List Application
+
+const path              = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+   entry: ['./src/index.html', './src/js/app.js'],
+   output: {
+      filename: 'bundle.js',
+      path:     path.resolve(__dirname, 'dist')
+      },
+   module: {
+      rules: [
+         { test: /\.css$/, use: ExtractTextPlugin.extract({ use: 'css-loader' }) },
+         { test: /\.html$/, loader: 'raw-loader' }
+         ]
+      },
+   plugins: [
+      new ExtractTextPlugin('bundle.css'),
+      new HtmlWebpackPlugin({ template: 'src/index.html' })
+      ]
+   };
